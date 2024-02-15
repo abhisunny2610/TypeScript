d234c7da4079128035e22bad20db851d51785e8c
Abhishek <abhisheksunny463@gmail.com>
2024-02-16 00:41:18 +0530

    Explore TypeScript

--- 01 Intro/variables.js
+++ 01 Intro/variables.js
@@ -1,2 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Good Morning, Abhishek 👋";
+// error occur 'number' is not assignable to type 'string
+// greetings = 1
 console.log(greetings);
+var mynum = 6;
+// mynum.toUpperCase()
+// number
+var user_id = 453822;
+// user_id.
+var muId = 98765;
+// typescript in very smart they autometically detect the data type of muid
+// muId = "Abhishek"
+// boolean
+var isLoggiedIn = false;
--- 01 Intro/variables.ts
+++ 01 Intro/variables.ts
@@ -9,4 +9,20 @@ let mynum = 6
 
 // mynum.toUpperCase()
 
+// number
+
+let user_id: number = 453822
+
+// user_id.
+
+let muId = 98765
+
+// typescript in very smart they autometically detect the data type of muid
+// muId = "Abhishek"
+
+// boolean
+let isLoggiedIn: boolean = false
+
+// isLoggiedIn
+
 export {}
