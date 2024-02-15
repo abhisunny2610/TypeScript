18f1ea2a288ea253ef558a0d5a94b662b0ce05ad
Abhishek <abhisheksunny463@gmail.com>
2024-02-16 00:50:11 +0530

    Explore TypeScript

--- 02 Basics/myfunctions.js
+++ 02 Basics/myfunctions.js
@@ -1,7 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function addTwo(num) {
-    num.toUpperCase();
     return num + 2;
 }
+// addTwo("5")
 addTwo(5);
+function getUpper(val) {
+    return val.toUpperCase();
+}
+// getUpper(4)
+getUpper("abhishek");
+function signUpUser(name, email, password) {
+}
+signUpUser("abhishek", "abhishek@gmail.com", "12345678");
+var loginUser = function (email, password, name) {
+    if (name === void 0) { name = "abhishek"; }
+};
+loginUser("abhishek@gmail.com", "12345678");
--- 02 Basics/myfunctions.ts
+++ 02 Basics/myfunctions.ts
@@ -5,4 +5,23 @@ function addTwo(num: number){
 // addTwo("5")
 addTwo(5)
 
+function getUpper (val: string){
+    return val.toUpperCase()
+}
+
+// getUpper(4)
+getUpper("abhishek")
+
+
+function signUpUser(name: string, email: string, password: string){
+
+}
+
+signUpUser("abhishek", "abhishek@gmail.com", "12345678")
+
+const loginUser = (email: string, password: string, name: string = "abhishek") => {}
+
+loginUser("abhishek@gmail.com", "12345678")
+
+
 export {}
